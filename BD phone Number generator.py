7a0b07349390b6d66f090ffc1726d395ddd94a7a
shuvoprime <srakibulalam@gmail.com>
2022-04-02 17:57:47 +0600

    refactoring code

--- BD phone Number generator.py
+++ BD phone Number generator.py
@@ -7,6 +7,14 @@ def random_phone_num_generator():
         rest.append(random.randint(0, 9))
     return mno_serial + "".join(map(str, rest))
  
-n = int(input("HOW MANY PHONE NUMBERS YOU WANT TO GENERATE?3:  "))
-for i in range(0,n):
+while True:
+    try:
+        n = int(input("HOW MANY PHONE NUMBERS YOU WANT TO GENERATE?:  "))
+        break
+    except ValueError:
+        print("Are you JOKING???  it must be an integer!")
+
+print(" Randomlly Printing "+ str(n) +" numbers which contains unused MNO = 011 ")
+
+for i in range(0,n): 
     print(random_phone_num_generator())
